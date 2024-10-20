6871d813541c6ac79577459a027eed1e75296e46
Advita Gelli <gelliadvita@gmail.com>
2024-10-20 15:01:11 -0500

    First pass at calendar

--- home/models.py
+++ home/models.py
@@ -1,22 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime
-from django.urls import reverse
-
-
-class Event(models.Model):
-  title = models.CharField(max_length=200)
-  start_time = models.DateTimeField(default=datetime.date.today)
-def __str__(self):
-  return self.title
-@property
-def get_html_url(self):
-  url = reverse('event_edit', args=(self.id,))
-  return f'<p>{self.title}</p><a href="{url}">edit</a>'
-
-
-
-
 
 class UserCycleData(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cycle_data')
--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
-from django.urls import include, re_path
-# from django.urls import url
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    re_path(r'^calendar/$', views.CalendarView.as_view(), name='calendar')
-    # url(r’^calendar/$’, views.CalendarView.as_view(), name=’calendar’)
 ]
