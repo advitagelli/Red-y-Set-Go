22c7ed8e964126b0690a0b02d5a7867630f18607
Advita Gelli <gelliadvita@gmail.com>
2024-10-20 14:45:24 -0500

    Refresh i will do

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.urls import include, re_path
+# from django.urls import url
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    re_path(r'^calendar/$', views.CalendarView.as_view(), name='calendar')
+    # url(r’^calendar/$’, views.CalendarView.as_view(), name=’calendar’)
 ]
--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import UserCycleDataForm
 from .models import UserCycleData
+from .utils import Calendar
 
 @login_required
 def home(request):
@@ -17,3 +18,17 @@ def home(request):
         form = UserCycleDataForm(instance=previous_response)
 
     return render(request, 'home.html', {'form': form})
+
+class CalendarView(ListView):
+    model = Event
+    template_name = 'components/calendar.html'
+    success_url = reverse_lazy("calendar")
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        d = get_date(self.request.GET.get('month', None))
+        cal = Calendar(d.year, d.month)
+        html_cal = cal.formatmonth(withyear=True)
+        context['calendar'] = mark_safe(html_cal)
+        context['prev_month'] = prev_month(d)
+        context['next_month'] = next_month(d)
+        return context
